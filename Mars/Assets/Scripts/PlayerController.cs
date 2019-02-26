c11394fa3d62cd7b63753c1087991e300d6a8837
konrad <konrad@polarczyk.com>
2019-02-26 21:23:31 +0100

    Kamera

--- Mars/Assets/Scripts/PlayerController.cs
+++ Mars/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    public float sensitivity;
+    private Camera cam;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        cam = GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Transform tr = transform;
+        transform.RotateAround(tr.position, tr.up, mouseX*sensitivity);
+        cam.transform.RotateAround(cam.transform.position, -tr.right, mouseY*sensitivity);
     }
 }
