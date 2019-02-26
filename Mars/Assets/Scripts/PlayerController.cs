1da05f60c77f612995f2959303733961b235a498
KonradPolarczyk <konradpolarczyk98@gmail.com>
2019-02-26 23:51:27 +0100

    PlayerController
    Player controller zawiera funkcje camLook, move, jump. Podstawowe poruszanie i limity kamery.

--- Mars/Assets/Scripts/PlayerController.cs
+++ Mars/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+	public float sensitivity;
+	public float moveSpeed;
+	public float jumpHeight;
+	public float angleLimit;
+	
+	private Camera cam;
+	private Rigidbody rb;
+	private Transform tr;
+	private CapsuleCollider col;
 
-    private Camera cam;
-    public float sensitivity;
-    // Start is called before the first frame update
-    void Start()
-    {
-        cam = GetComponentInChildren<Camera>();
-    }
+	// Start is called before the first frame update
+	private void Start() {
+		cam = GetComponentInChildren<Camera>();
+		rb = GetComponent<Rigidbody>();
+		tr = transform;
+		col = GetComponent<CapsuleCollider>();
+		
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 
-    // Update is called once per frame
-    void Update() {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+	private void Update() {
+		camLook();
+		jump();
+	}
 
-        Transform tr = transform;
-        transform.RotateAround(tr.position, tr.up, mouseX*sensitivity);
-        cam.transform.RotateAround(cam.transform.position, -tr.right, mouseY*sensitivity);
-    }
+	private void FixedUpdate() {
+		move();
+	}
+
+	bool isGrounded() {
+		return Physics.Raycast(tr.TransformPoint(col.center), -Vector3.up, out var hitInfo, col.height / 2 + 0.1f);
+	}
+
+	void camLook() {
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+		tr.RotateAround(tr.position, tr.up, mouseX * sensitivity);
+
+		float currentAngle = Vector3.Angle(-tr.up, cam.transform.forward) - 90;
+		float angle = mouseY * sensitivity;
+		
+		if (Mathf.Abs(currentAngle + angle) < angleLimit)
+			cam.transform.RotateAround(cam.transform.position, -cam.transform.right, angle);
+	}
+
+	void move() {
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
+
+		Vector3 moveVector = moveX * tr.right + moveY * tr.forward;
+
+		rb.velocity = moveVector * Time.deltaTime * moveSpeed * 100 + rb.velocity.y * tr.up;
+	}
+
+	void jump() {
+		if (Input.GetButtonDown("Jump") && isGrounded()) {
+			rb.velocity += Vector3.up * jumpHeight;
+		}
+	}
 }
