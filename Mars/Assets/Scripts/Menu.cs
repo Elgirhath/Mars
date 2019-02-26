37cd23ddb02411463a42db7807ad5c0f28f574d0
Oskar <pawicao@gmail.com>
2019-02-26 23:48:23 +0100

    PlayerController - Oskar
    Skakanie, ruch kamery i poruszanie się.

--- Mars/Assets/Scripts/Menu.cs
+++ Mars/Assets/Scripts/Menu.cs
@@ -1,31 +1,38 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour
+{
 	// GAMEOBJECT
 
 	[MenuItem("GameObject/Toggle Hide _h")] // toggle hide
-	private static void ToggleHideSelection() {
+	private static void ToggleHideSelection()
+	{
 		var selectedObjects = Selection.gameObjects;
-		foreach (var obj in selectedObjects) {
+		foreach (var obj in selectedObjects)
+		{
 			Undo.RecordObject(obj, "Toggle Hide");
 			obj.SetActive(!obj.activeSelf);
 		}
 	}
 
 	[MenuItem("GameObject/Unhide all &h")] // unhide all
-	private static void UnhideAll() {
+	private static void UnhideAll()
+	{
 		var allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
-		foreach (var obj in allObjects) {
+		foreach (var obj in allObjects)
+		{
 			Undo.RecordObject(obj, "Unhide all");
 			obj.SetActive(true);
 		}
 	}
 
 	[MenuItem("GameObject/Reset transform &r")] // reset transform
-	private static void ResetTransform() {
+	private static void ResetTransform()
+	{
 		var selectedObjects = Selection.gameObjects;
-		foreach (var obj in selectedObjects) {
+		foreach (var obj in selectedObjects)
+		{
 			Undo.RecordObject(obj.transform, "Reset transform");
 			obj.transform.position = Vector3.zero;
 		}
@@ -33,18 +40,22 @@ public class Menu : MonoBehaviour {
 
 
 	[MenuItem("GameObject/Snap/To pivot #s")] // reset transform
-	private static void SnapToPivot() {
+	private static void SnapToPivot()
+	{
 		var selectedObjects = Selection.gameObjects;
-		foreach (var obj in selectedObjects) {
+		foreach (var obj in selectedObjects)
+		{
 			Undo.RecordObject(obj.transform, "Reset transform");
 			obj.transform.position = SceneView.lastActiveSceneView.pivot;
 		}
 	}
 
 	[MenuItem("GameObject/Snap/To grid &#s")] // reset transform
-	private static void SnapToGrid() {
+	private static void SnapToGrid()
+	{
 		var selectedObjects = Selection.gameObjects;
-		foreach (var obj in selectedObjects) {
+		foreach (var obj in selectedObjects)
+		{
 			Undo.RecordObject(obj.transform, "Reset transform");
 			var rounded = Vector3Int.RoundToInt(obj.transform.position);
 			obj.transform.position = rounded;
@@ -54,45 +65,53 @@ public class Menu : MonoBehaviour {
 	// SCENE VIEW NAVIGATION
 
 	[MenuItem("View/Switch Perspective _5")]
-	private static void SwitchPerspective() {
+	private static void SwitchPerspective()
+	{
 		SceneView.lastActiveSceneView.orthographic = !SceneView.lastActiveSceneView.orthographic; //toggle
 
 		SceneView.lastActiveSceneView.Repaint();
 	}
 
-	private static void rotateView(Quaternion rotation) {
+	private static void rotateView(Quaternion rotation)
+	{
 		SceneView.lastActiveSceneView.LookAt(SceneView.lastActiveSceneView.pivot, rotation);
 
 		SceneView.lastActiveSceneView.Repaint();
 	}
 
 	[MenuItem("View/Front _1")] // FRONT
-	private static void FrontView() {
+	private static void FrontView()
+	{
 		rotateView(Quaternion.Euler(0, 180, 0));
 	}
 
 	[MenuItem("View/Back &1")] // BACK
-	private static void BackView() {
+	private static void BackView()
+	{
 		rotateView(Quaternion.Euler(0, 0, 0));
 	}
 
 	[MenuItem("View/Right _2")] // RIGHT
-	private static void RightView() {
+	private static void RightView()
+	{
 		rotateView(Quaternion.Euler(0, -90, 0));
 	}
 
 	[MenuItem("View/Left &2")] // LEFT
-	private static void LeftView() {
+	private static void LeftView()
+	{
 		rotateView(Quaternion.Euler(0, 90, 0));
 	}
 
 	[MenuItem("View/Top _3")] // TOP
-	private static void TopView() {
+	private static void TopView()
+	{
 		rotateView(Quaternion.Euler(90, 0, 0));
 	}
 
 	[MenuItem("View/Bottom &3")] // BOTTOM
-	private static void BottomView() {
+	private static void BottomView()
+	{
 		rotateView(Quaternion.Euler(-90, 0, 0));
 	}
 }
--- Mars/Assets/Scripts/PlayerController.cs
+++ Mars/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,21 @@
+<<<<<<< HEAD
 ﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+=======
+﻿using TMPro;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+>>>>>>> 5c42195... PlayerController - Oskar
 	public float sensitivity;
 	public float moveSpeed;
 	public float jumpHeight;
 	public float angleLimit;
 	
 	private Camera cam;
+<<<<<<< HEAD
 	private Rigidbody rb;
 	private Transform tr;
 	private CapsuleCollider col;
@@ -63,3 +72,52 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 }
+=======
+	private Rigidbody rBody;
+	private Transform tr;
+	private CapsuleCollider col;
+
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		cam = GetComponentInChildren<Camera>();
+		tr = transform;
+		rBody = GetComponent<Rigidbody>();
+		col = GetComponent<CapsuleCollider>();
+	}
+
+	private void Update()
+	{
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+		float angle = mouseY * sensitivity;
+		float currentAngle = Vector3.Angle(-tr.up, cam.transform.forward) - 90;
+
+		
+		transform.RotateAround(tr.position, tr.up, mouseX * sensitivity);
+		if (Mathf.Abs(currentAngle + angle) < angleLimit)
+		{
+			cam.transform.RotateAround(cam.transform.position, -cam.transform.right, angle);
+		}
+		
+		// Jumping
+		if (Input.GetButtonDown("Jump") && Physics.Raycast(tr.TransformPoint(col.center), -Vector3.up, out var hitInfo,col.height/2 + 0.1f))
+		{
+			rBody.velocity = Vector3.up * jumpHeight;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		// Player movement
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
+		Vector3 moveVector = moveX * tr.right + moveY * tr.forward;
+		rBody.velocity = moveVector.normalized * Time.deltaTime * moveSpeed * 100 + tr.up * rBody.velocity.y;
+
+	}
+}
+>>>>>>> 5c42195... PlayerController - Oskar
