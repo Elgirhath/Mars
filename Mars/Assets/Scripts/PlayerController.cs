5c421954381d1109f5730865ec3a2ee44f4f2e24
Oskar <pawicao@gmail.com>
2019-02-26 23:48:23 +0100

    PlayerController - Oskar
    Skakanie, ruch kamery i poruszanie się.

--- Mars/Assets/Scripts/PlayerController.cs
+++ Mars/Assets/Scripts/PlayerController.cs
@@ -1,22 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Numerics;
+﻿using TMPro;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    public float sensitivity;
-    private Camera cam;
-    // Start is called before the first frame update
-    void Start() {
-        cam = GetComponentInChildren<Camera>();
-    }
-    
-    void Update() {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+public class PlayerController : MonoBehaviour
+{
+	public float sensitivity;
+	public float moveSpeed;
+	public float jumpHeight;
+	public float angleLimit;
+	
+	private Camera cam;
+	private Rigidbody rBody;
+	private Transform tr;
+	private CapsuleCollider col;
 
-        Transform tr = transform;
-        transform.RotateAround(tr.position, tr.up, mouseX*sensitivity);
-        cam.transform.RotateAround(cam.transform.position, -tr.right, mouseY*sensitivity);
-    }
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+		cam = GetComponentInChildren<Camera>();
+		tr = transform;
+		rBody = GetComponent<Rigidbody>();
+		col = GetComponent<CapsuleCollider>();
+	}
+
+	private void Update()
+	{
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+		float angle = mouseY * sensitivity;
+		float currentAngle = Vector3.Angle(-tr.up, cam.transform.forward) - 90;
+
+		
+		transform.RotateAround(tr.position, tr.up, mouseX * sensitivity);
+		if (Mathf.Abs(currentAngle + angle) < angleLimit)
+		{
+			cam.transform.RotateAround(cam.transform.position, -cam.transform.right, angle);
+		}
+		
+		// Jumping
+		if (Input.GetButtonDown("Jump") && Physics.Raycast(tr.TransformPoint(col.center), -Vector3.up, out var hitInfo,col.height/2 + 0.1f))
+		{
+			rBody.velocity = Vector3.up * jumpHeight;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		// Player movement
+		float moveX = Input.GetAxisRaw("Horizontal");
+		float moveY = Input.GetAxisRaw("Vertical");
+		Vector3 moveVector = moveX * tr.right + moveY * tr.forward;
+		rBody.velocity = moveVector.normalized * Time.deltaTime * moveSpeed * 100 + tr.up * rBody.velocity.y;
+
+	}
 }
