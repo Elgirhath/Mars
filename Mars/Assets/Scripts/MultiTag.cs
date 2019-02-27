0e89857b10af346e039f6f6da6bc3906e3472cbe
KonradPolarczyk <konradpolarczyk98@gmail.com>
2019-02-27 21:47:11 +0100

    MultiTag
    Stworzone MultiTagi

---
+++ Mars/Assets/Scripts/MultiTag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTag : MonoBehaviour {
+	public string[] tags;
+
+	public bool Contains(string tag) {
+		int pos = Array.IndexOf(tags, tag);
+		return pos > -1;
+	}
+}
