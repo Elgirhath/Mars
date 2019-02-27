a300c70e448efd8d989472e32c670c65b5ef6d17
konrad <konrad@polarczyk.com>
2019-02-27 22:26:08 +0100

    Poprawione ItemCollecting
    Działający hitscan

--- Mars/Assets/Scripts/ItemCollectingController.cs
+++ Mars/Assets/Scripts/ItemCollectingController.cs
@@ -2,52 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemCollectingController : MonoBehaviour
-{
-    public float maxCollectDistance;
+public class ItemCollectingController : MonoBehaviour {
+	public float maxCollectDistance;
 
-    private Camera cam;
-    private bool allowCollecting;
-    private GameObject target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        cam = GetComponentInChildren<Camera>();
-    }
+	private Camera cam;
+	private bool allowCollecting;
 
-    // Update is called once per frame
-    void Update()
-    {
-        allowCollecting = false;
-        RaycastHit hit = new RaycastHit();
-        if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit))
-        {
-            try
-            {
-                if (hit.transform.gameObject.GetComponent<MultiTag>().Contains("Collectible"))
-                    //if(hit.transform.gameObject.CompareTag("Collectible"))    
-                {
-                    if (hit.distance <= maxCollectDistance)
-                    {
-                        target = hit.transform.gameObject;
-                        allowCollecting = true;
-                    }
-                }
-            }
-            catch (UnityException)
-            {
-                
-            }
-        }
+	private GameObject target;
 
-        if (allowCollecting)
-        {
-            if (Input.GetButtonDown("Use"))
-            {
-                Destroy(target);
-                Debug.Log("Dodano do ekwipunku");
-            }
-        }
-        
-    }
+	// Start is called before the first frame update
+	void Start() {
+		cam = GetComponentInChildren<Camera>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		allowCollecting = false;
+		RaycastHit hit = new RaycastHit();
+		if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit)) {
+			try {
+				if (hit.transform.gameObject.GetComponent<MultiTag>().Contains("Collectible"))
+					//if(hit.transform.gameObject.CompareTag("Collectible"))    
+				{
+					if (hit.distance <= maxCollectDistance) {
+						target = hit.transform.gameObject;
+						allowCollecting = true;
+					}
+				}
+			}
+			catch {}
+		}
+
+		if (allowCollecting) {
+			if (Input.GetButtonDown("Use")) {
+				Destroy(target);
+				Debug.Log("Dodano do ekwipunku");
+			}
+		}
+	}
 }
