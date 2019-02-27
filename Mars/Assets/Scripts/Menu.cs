85794a764342006cca0e0b92b36b5543f62f006c
konrad <konrad@polarczyk.com>
2019-02-27 20:09:17 +0100

    Poprawione Menu.cs
    Funkcje nie są już uruchamiane w Game mode

--- Mars/Assets/Scripts/Menu.cs
+++ Mars/Assets/Scripts/Menu.cs
@@ -2,40 +2,37 @@
 using UnityEditor;
 using UnityEngine;
 
-public class Menu : MonoBehaviour
-{
+public class Menu : MonoBehaviour {
 	// GAMEOBJECT
 
 	[MenuItem("GameObject/Toggle Hide _h")] // toggle hide
-	private static void ToggleHideSelection()
-	{
-		var selectedObjects = Selection.gameObjects;
-		foreach (var obj in selectedObjects)
-		{
+	private static void ToggleHideSelection() {
+		if (!isInEditMode())
+			return;
+		GameObject[] selectedObjects = Selection.gameObjects;
+		foreach (GameObject obj in selectedObjects) {
 			Undo.RecordObject(obj, "Toggle Hide");
 			obj.SetActive(!obj.activeSelf);
 		}
 	}
 
 	[MenuItem("GameObject/Unhide all &h")] // unhide all
-	private static void UnhideAll()
-	{
-		var allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
-		foreach (var obj in allObjects)
-		{
+	private static void UnhideAll() {
+		if (!isInEditMode())
+			return;
+		GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+		foreach (GameObject obj in allObjects) {
 			Undo.RecordObject(obj, "Unhide all");
 			obj.SetActive(true);
 		}
 	}
 
 	[MenuItem("GameObject/Reset transform &r")] // reset transform
-	private static void ResetTransform()
-	{
-		if (!Application.isEditor)
-			throw new Exception();
-		var selectedObjects = Selection.gameObjects;
-		foreach (var obj in selectedObjects)
-		{
+	private static void ResetTransform() {
+		if (!isInEditMode())
+			return;
+		GameObject[] selectedObjects = Selection.gameObjects;
+		foreach (GameObject obj in selectedObjects) {
 			Undo.RecordObject(obj.transform, "Reset transform");
 			obj.transform.position = Vector3.zero;
 		}
@@ -44,22 +41,23 @@ public class Menu : MonoBehaviour
 
 	[MenuItem("GameObject/Snap/To pivot #s")] // snap to pivot
 	private static void SnapToPivot() {
-		var selectedObjects = Selection.gameObjects;
-		foreach (var obj in selectedObjects)
-		{
+		if (!isInEditMode())
+			return;
+		GameObject[] selectedObjects = Selection.gameObjects;
+		foreach (GameObject obj in selectedObjects) {
 			Undo.RecordObject(obj.transform, "Snap to pivot");
 			obj.transform.position = SceneView.lastActiveSceneView.pivot;
 		}
 	}
 
 	[MenuItem("GameObject/Snap/To grid &#s")] // snap to grid
-	private static void SnapToGrid()
-	{
-		var selectedObjects = Selection.gameObjects;
-		foreach (var obj in selectedObjects)
-		{
+	private static void SnapToGrid() {
+		if (!isInEditMode())
+			return;
+		GameObject[] selectedObjects = Selection.gameObjects;
+		foreach (GameObject obj in selectedObjects) {
 			Undo.RecordObject(obj.transform, "Snap to grid");
-			var rounded = Vector3Int.RoundToInt(obj.transform.position);
+			Vector3Int rounded = Vector3Int.RoundToInt(obj.transform.position);
 			obj.transform.position = rounded;
 		}
 	}
@@ -67,53 +65,65 @@ public class Menu : MonoBehaviour
 	// SCENE VIEW NAVIGATION
 
 	[MenuItem("View/Switch Perspective _5")]
-	private static void SwitchPerspective()
-	{
+	private static void SwitchPerspective() {
+		if (!isInEditMode())
+			return;
 		SceneView.lastActiveSceneView.orthographic = !SceneView.lastActiveSceneView.orthographic; //toggle
 
 		SceneView.lastActiveSceneView.Repaint();
 	}
 
-	private static void rotateView(Quaternion rotation)
-	{
+	private static void rotateView(Quaternion rotation) {
+		if (!isInEditMode())
+			return;
 		SceneView.lastActiveSceneView.LookAt(SceneView.lastActiveSceneView.pivot, rotation);
 
 		SceneView.lastActiveSceneView.Repaint();
 	}
 
 	[MenuItem("View/Front _1")] // FRONT
-	private static void FrontView()
-	{
+	private static void FrontView() {
+		if (!isInEditMode())
+			return;
 		rotateView(Quaternion.Euler(0, 180, 0));
 	}
 
 	[MenuItem("View/Back &1")] // BACK
-	private static void BackView()
-	{
+	private static void BackView() {
+		if (!isInEditMode())
+			return;
 		rotateView(Quaternion.Euler(0, 0, 0));
 	}
 
 	[MenuItem("View/Right _2")] // RIGHT
-	private static void RightView()
-	{
+	private static void RightView() {
+		if (!isInEditMode())
+			return;
 		rotateView(Quaternion.Euler(0, -90, 0));
 	}
 
 	[MenuItem("View/Left &2")] // LEFT
-	private static void LeftView()
-	{
+	private static void LeftView() {
+		if (!isInEditMode())
+			return;
 		rotateView(Quaternion.Euler(0, 90, 0));
 	}
 
 	[MenuItem("View/Top _3")] // TOP
-	private static void TopView()
-	{
+	private static void TopView() {
+		if (!isInEditMode())
+			return;
 		rotateView(Quaternion.Euler(90, 0, 0));
 	}
 
 	[MenuItem("View/Bottom &3")] // BOTTOM
-	private static void BottomView()
-	{
+	private static void BottomView() {
+		if (!isInEditMode())
+			return;
 		rotateView(Quaternion.Euler(-90, 0, 0));
+	}
+
+	private static bool isInEditMode() {
+		return !Application.isPlaying;
 	}
 }
