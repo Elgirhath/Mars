4198a024ed23571eef84d1ceb84c206be7b282f1
konrad <konrad@polarczyk.com>
2019-02-27 23:58:18 +0100

    Placing items
    Przezroczyste schematy + lerp. Problemy z materiałami

--- Mars/Assets/Scripts/BuildController.cs
+++ Mars/Assets/Scripts/BuildController.cs
@@ -4,33 +4,68 @@ using UnityEngine;
 
 public class BuildController : MonoBehaviour {
 	public GameObject prefab;
+	public float maxDistance;
+	public float rotLerpSpeed;
+	public Material schemeMaterial;
 
-	private Camera cam;
+	private Material defaultMaterial;
+	private Transform cam;
 	private bool isPlacing;
 	private GameObject scheme;
 	private TerrainData terrain;
+
+	private Vector3 placePosition;
 	
 	// Update is called once per frame
 	void Start() {
 		isPlacing = false;
-		cam = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Camera>();
+		cam = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Camera>().transform;
 		terrain = Terrain.activeTerrain.terrainData;
+		placePosition = Vector3.zero;
+		try {
+			defaultMaterial = scheme.GetComponent<Renderer>().material;
+		}
+		catch {}
 	}
 	
 	void Update() {
-		RaycastHit hitInfo;
-		Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo);
+		bool wasHit = Physics.Raycast(cam.position, cam.forward, out RaycastHit hitInfo);
 
-		Vector3 placePosition = hitInfo.point;
+		bool isInRange = Vector3.Distance(hitInfo.point, cam.position) < maxDistance;
+
+		placePosition = wasHit && isInRange ? hitInfo.point : placePosition;
 		
 		if (Input.GetKeyDown(KeyCode.Q) && !isPlacing) {
 			isPlacing = true;
-			scheme = Instantiate(prefab, placePosition, Quaternion.Euler(0, 0, 0));
+			scheme = Instantiate(prefab, placePosition, Quaternion.identity);
+			scheme.transform.up = wasHit && isInRange ? hitInfo.normal : scheme.transform.up;
+
+			try {
+				scheme.GetComponent<Collider>().enabled = false;
+				scheme.GetComponent<Renderer>().material = schemeMaterial;
+			}
+			catch { }
 		}
 
 		if (isPlacing) {
 			scheme.transform.position = placePosition;
-			scheme.transform.rotation = Quaternion.Euler(0, 0, 0);
+			Vector3 oldUpVector = scheme.transform.up;
+			Vector3 newUpVector = wasHit && isInRange ? hitInfo.normal : oldUpVector;
+
+			float angle = Vector3.Angle(oldUpVector, newUpVector);
+
+			scheme.transform.up = Vector3.Lerp(oldUpVector, newUpVector, rotLerpSpeed * Time.deltaTime);
+
+			if (Input.GetButtonDown("Fire1")) {
+				try {
+					scheme.GetComponent<Collider>().enabled = true;
+					scheme.GetComponent<Renderer>().material = defaultMaterial;
+				}
+				catch {}
+				
+				scheme = null;
+				isPlacing = false;
+			}
 		}
 	}
 }
