e0bff4c747e8f224be6c0568df7839aaa4c217dc
konrad <konrad@polarczyk.com>
2019-02-28 22:35:46 +0100

    Poprawione ItemCollectingController i BuildController
    Zmienione liczenie wag i pola serializble oraz dodana klasa BuildingState do BuildController

---
+++ Mars/Assets/Scripts/ArchRaycast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities {
+	public static bool ArchRaycast(Vector3 origin, float radius, Vector3 startDir, Vector3 endDir, out RaycastHit hit, float anglePrecision) {
+		Vector3 axis = Vector3.Cross(startDir, endDir);
+		float angle = Vector3.Angle(startDir, endDir);
+		float currentAngle = 0.0f;
+		Vector3 or = startDir.normalized * radius;
+		while (currentAngle < angle) {
+			Vector3 end = Quaternion.AngleAxis(anglePrecision, axis) * or;
+			Ray ray = new Ray(or + origin, end - or);
+			Debug.DrawRay(origin, startDir, Color.yellow);
+			Debug.DrawRay(ray.origin, ray.direction, Color.red);
+			Debug.DrawRay(origin, axis, Color.blue);
+			RaycastHit rayHit;
+			bool wasHit = Physics.Raycast(ray, out rayHit, (end - or).magnitude);
+			if (wasHit) {
+				Debug.DrawRay(rayHit.point, Vector3.up, Color.green);
+				hit = rayHit;
+				return true;
+			}
+
+			or = end;
+			currentAngle += anglePrecision;
+		}
+
+		hit = new RaycastHit();
+		return false;
+	}
+}
--- Mars/Assets/Scripts/ItemCollectingController.cs
+++ Mars/Assets/Scripts/ItemCollectingController.cs
@@ -3,51 +3,55 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ItemCollectingController : MonoBehaviour {
+	[Tooltip("Max distance from camera in which objects can be collected")]
 	public float maxCollectDistance;
-	public float magnitudeWeight;
-	public float angleWeight;
+	[Tooltip("Weight from 0 to 1. 0 means distance only, 1 means angle only.")]
+	[Range(0.0f, 1.0f)]
+	public float weight;
+	[Tooltip("Max angle from camera forward in which objects can be collected")]
+	[Range(0.0f, 90.0f)]
 	public float maxAngle;
 
-	private Camera cam;
+	private float distanceWeight;
+	private float angleWeight;
+	private Transform camTransform;
 	private bool allowCollecting;
 	private Collider[] nearItems;
 	private List<Tuple<Transform, float>> itemsInFront = new List<Tuple<Transform, float>>();
 	private GameObject target;
 
 	void Start() {
-		cam = GetComponentInChildren<Camera>();
+		camTransform = GetComponentInChildren<Camera>().transform;
 	}
 
 	void Update() {
 		allowCollecting = false;
 		itemsInFront.Clear();
-		
-		nearItems = Physics.OverlapSphere(transform.position, maxCollectDistance);
-		for (int i = 0; i < nearItems.Length; ++i)
-		{
-			Vector3 itemVector = nearItems[i].transform.position - transform.position;
-			float angle = Vector3.Angle(itemVector, transform.forward);
+
+		nearItems = Physics.OverlapSphere(transform.position, maxCollectDistance); //get all objects in range
+		for (int i = 0; i < nearItems.Length; ++i) {
+			Vector3 camItemVector = nearItems[i].transform.position - camTransform.position; //vector from camera to the object
+			float angle = Vector3.Angle(camItemVector, camTransform.forward);
 			if (angle > maxAngle)
 				continue;
-			try
-			{
-				if (nearItems[i].transform.gameObject.GetComponent<MultiTag>().Contains("Collectible"))
-				{
-					itemsInFront.Add(new Tuple<Transform, float>(nearItems[i].transform,
-						magnitudeWeight * itemVector.magnitude + angleWeight * angle));
+			try {
+				if (nearItems[i].transform.gameObject.GetComponent<MultiTag>().Contains("Collectible")) {
+					float factor = weight * camItemVector.magnitude + (1 - weight) * angle; //selecting the right object by the lowest factor
+					itemsInFront.Add(new Tuple<Transform, float>(nearItems[i].transform, factor));
 				}
 			}
 			catch {}
 		}
+
 		itemsInFront.Sort((a, b) => a.Item2.CompareTo(b.Item2));
-		if (itemsInFront.Count > 0)
-		{
+		if (itemsInFront.Count > 0) {
 			allowCollecting = true;
 			target = itemsInFront.First().Item1.gameObject;
 		}
-		Debug.Log(itemsInFront.Count);
+//		Debug.Log(itemsInFront.Count);
 
 		//OLD HITCAST
 		/*
