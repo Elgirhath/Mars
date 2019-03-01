946cd2568c57cc8ad5bb52e286b9ea25e4814279
konrad <konrad@polarczyk.com>
2019-03-01 16:12:40 +0100

    MultiObject building schemes

--- Mars/Assets/Scripts/BuildController.cs
+++ Mars/Assets/Scripts/BuildController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
@@ -16,11 +17,13 @@ public class BuildController : MonoBehaviour {
 	private Transform cam;
 	private bool isPlacing;
 	private GameObject scheme;
-	private BuildingState savedState;
+	private ObjectTreeState savedState;
 	
 	private float schemeYRotation;
 	private Vector3 placePosition;
 	private int schemeLayer;
+	
+	
 
 	// Update is called once per frame
 	void Start() {
@@ -81,42 +84,89 @@ public class BuildController : MonoBehaviour {
 		}
 	}
 
-	private BuildingState SavePrefabState(GameObject obj) {
-		return new BuildingState(obj);
+	private ObjectTreeState SavePrefabState(GameObject obj) {
+		return new ObjectTreeState(obj);
 	}
 
 	private void setSchemeState(GameObject obj) {
-		BuildingState schemeState = new BuildingState {
+		ObjectState schemeState = new ObjectState {
 			material = schemeMaterial,
 			colliderEnable = false,
 			layer = schemeLayer
 		};
 
-		schemeState.apply(obj);
+		ObjectTreeState.apply(obj, schemeState);
 	}
 }
 
-public class BuildingState {
+public class ObjectTreeState { // Keeps ObjectState for every object in the subtree
+	private Dictionary<Transform, ObjectState> states = new Dictionary<Transform, ObjectState>();
+	
+	public ObjectTreeState() {}
+	
+	public ObjectTreeState(GameObject obj) : this() {
+		get(obj);
+	}
+	public void get(GameObject obj) {
+		ObjectState parentState = new ObjectState(obj);
+		states.Add(obj.transform, parentState);
+		foreach (Transform child in obj.transform) {
+			ObjectState childState = new ObjectState(child.gameObject);
+			states.Add(child, childState);
+		}
+	}
+	
+	public void apply(GameObject obj) {
+		states[obj.transform].apply(obj);
+		foreach (Transform child in obj.transform) {
+			try {
+				states[child].apply(child.gameObject);
+			}
+			catch {}
+		}
+	}
+	
+	public static void apply(GameObject obj, ObjectState state) {
+		state.apply(obj);
+		foreach (Transform child in obj.transform) {
+			state.apply(child.gameObject);
+		}
+	}
+}
+
+public class ObjectState { // Keeps object state which is changed while placing a build
 	//TODO: Multiobject states
 	public Material material;
 	public bool colliderEnable;
 	public int layer;
 
-	public BuildingState() {}
+	public ObjectState() {}
 	
-	public BuildingState(GameObject obj) : this() {
+	public ObjectState(GameObject obj) : this() {
 		get(obj);
 	}
 	
 	public void get(GameObject obj) {
-		material = obj.GetComponent<Renderer>().material;
-		colliderEnable = obj.GetComponent<Collider>().enabled;
+		try {
+			material = obj.GetComponent<Renderer>().material;
+		} catch{}
+
+		try {
+			colliderEnable = obj.GetComponent<Collider>().enabled;
+		} catch {}
+
 		layer = obj.layer;
 	}
 
 	public void apply(GameObject obj) {
-		obj.GetComponent<Renderer>().material = material;
-		obj.GetComponent<Collider>().enabled = colliderEnable;
+		try {
+			obj.GetComponent<Renderer>().material = material;
+		} catch {}
+
+		try {
+			obj.GetComponent<Collider>().enabled = colliderEnable;
+		} catch {}
+
 		obj.layer = layer;
 	}
 }
