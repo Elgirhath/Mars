057781d5b714b5738dde19016ea5eef294af8370
KonradPolarczyk <konradpolarczyk98@gmail.com>
2019-03-02 17:47:43 +0100

    Poprawione BuildMenu

--- Mars/Assets/Scripts/PlayerController.cs
+++ Mars/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour {
 	public float sensitivity;
 	private Transform tr;
 	public float walkSpeed;
+	private bool locked;
 
 // Start is called before the first frame update
 	private void Start() {
@@ -35,6 +36,9 @@ public class PlayerController : MonoBehaviour {
 
 	private void CamLook() {
 		//TODO: interpolate cam movement: http://www.kinematicsoup.com/news/2016/8/9/rrypp5tkubynjwxhxjzd42s3o034o8?fbclid=IwAR09F2udjxU9i_D7VuLsjHUXPco0tGQ4Gnf96emKsRcBNCt6ubW43xnlO-c
+
+		if (locked)
+			return;
 		
 		float mouseX = Input.GetAxis("Mouse X");
 		float mouseY = Input.GetAxis("Mouse Y");
@@ -74,5 +78,10 @@ public class PlayerController : MonoBehaviour {
 
 	private bool IsGrounded() {
 		return Physics.Raycast(tr.TransformPoint(col.center), -Vector3.up, col.height / 2 + 0.1f);
+	}
+
+	public bool camLockState {
+		get { return locked; }
+		set { locked = value; }
 	}
 }
