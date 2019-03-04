e3b99596876e042836bf23975a43afa2992a7f35
KonradPolarczyk <konradpolarczyk98@gmail.com>
2019-03-04 18:53:39 +0100

    Inventory
    Są podstawy, dodawanie do ekwipunku, z zapisem ważniejszych rzeczy, w tym funkcją onUse().
    Do poprawy: poprawne korzystanie z OnUse, uniwersalne dla wszystkich itemów.

--- Mars/Assets/Scripts/InventoryItem.cs
+++ Mars/Assets/Scripts/InventoryItem.cs
@@ -1,9 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
 
-public class InventoryItem : MonoBehaviour {
-	public bool collectible = true;
+public interface InventoryItem {
+	ItemProperties Properties { get; set; }
+}
+
+[Serializable]
+public class ItemProperties {
+	public bool collectible;
 	public string name;
 	public Sprite sprite;
+	public GameObject prefab;
+
+	public ItemUseEvent onUse; //called when an item is used with LMB in inventory
+
+	public ItemProperties() {}
+	public ItemProperties(ItemProperties other) : this() {
+		Set(other);
+	}
+
+	public void Set(ItemProperties other) {
+		collectible = other.collectible;
+		name = other.name;
+		sprite = other.sprite;
+		onUse = other.onUse;
+		prefab = other.prefab;
+	}
 }
+
+[Serializable]
+public class ItemUseEvent : UnityEngine.UI.Button.ButtonClickedEvent {}
---
+++ Mars/Assets/Scripts/SphereController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour, InventoryItem {
+	[SerializeField]
+	private ItemProperties properties;
+	public ItemProperties Properties {
+		get => properties;
+		set => properties = value;
+	}
+
+	private void Start() {
+		properties.onUse.AddListener(delegate { UseFunc(); });
+	}
+
+	private void UseFunc() {
+		Debug.Log("You used " + name);
+	}
+}
