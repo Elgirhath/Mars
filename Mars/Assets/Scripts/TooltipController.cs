94cedc1d2f2d15c856353981cac9e0e9f6a4692c
Oskar <pawicao@gmail.com>
2019-03-05 20:03:27 +0100

    TooltipController

---
+++ Mars/Assets/Scripts/TooltipController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipController : MonoBehaviour
+{
+    private string buttonName;
+    private string actionName;
+    
+    private static TooltipController instance;
+    public static TooltipController Instance => instance;
+    
+    
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+        }
+    }
+}
