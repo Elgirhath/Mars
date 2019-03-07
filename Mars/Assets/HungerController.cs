495f4119f884a89aad490d11540ebdb427c19108
KonradPolarczyk <konradpolarczyk98@gmail.com>
2019-03-07 18:03:35 +0100

    PlayerCondition
    W miarę opanowane pragnienie i głód. Dodany item WaterBottle, który nie działa

---
+++ Mars/Assets/HungerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HungerController : MonoBehaviour {
+    public int maxHunger;
+    public int initHunger;
+
+    private float _hunger;
+    public float hunger {
+        get => _hunger;
+        set => _hunger = value;
+    }
+	
+    [Tooltip("Points per second")]
+    public float dropSpeed;
+
+    public static HungerController instance;
+
+    private void Awake() {
+        if (!instance)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Start() {
+        _hunger = initHunger;
+    }
+
+    private void Update() {
+        _hunger -= dropSpeed * Time.deltaTime;
+
+        if (_hunger <= 0.0f) {
+            Debug.Log("U ded");
+        }
+    }
+}
---
+++ Mars/Assets/WaterBottle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBottle : MonoBehaviour, ItemController {
+    [SerializeField]
+    private Item _item;
+    public Item item {
+        get => _item;
+        set => _item = value;
+    }
+
+    private PlayerController playerController;
+
+    private void Start() {
+        _item.onUse.AddListener(Drink);
+        playerController = PlayerController.instance;
+    }
+
+    private void Drink() {
+        Debug.Log("You drunk a bottle of water!");
+        playerController.GetComponent<ThirstController>().thirst += 20;
+    }
+}
