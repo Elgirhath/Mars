b218ca5fb5e3ffae9df3484e9c67e4abe63732ea
konrad <konrad@polarczyk.com>
2019-03-08 16:54:40 +0100

    Water bottle item
    Works

--- Mars/Assets/Items/Item.cs
+++ Mars/Assets/Items/Item.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using UnityEngine;
 
-public interface ItemController {
-	[SerializeField]
-	Item item { get; set; }
-}
-
-[CreateAssetMenu(fileName = "NewItem", menuName="Item", order = 81)]
-public class Item : ScriptableObject {
+public abstract class Item : ScriptableObject {
+	[Space(10)]
+	[Header("Item properties")]
 	public bool collectible;
 	public string itemName;
 	public Sprite sprite;
 	public GameObject prefab;
 	public int stackLimit;
-
-	public ItemUseEvent onUse; //called when an item is used with LMB in inventory
 
 	public Item() {}
 	public Item(Item other) : this() {
@@ -25,11 +19,9 @@ public class Item : ScriptableObject {
 		collectible = other.collectible;
 		itemName = other.itemName;
 		sprite = other.sprite;
-		onUse = other.onUse;
 		prefab = other.prefab;
 		stackLimit = other.stackLimit;
 	}
-}
 
-[Serializable]
-public class ItemUseEvent : UnityEngine.UI.Button.ButtonClickedEvent {}
+	public abstract void Use();
+}
--- Mars/Assets/WaterBottle.cs
+++ Mars/Assets/WaterBottle.cs
@@ -1,24 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class WaterBottle : MonoBehaviour, ItemController {
-    [SerializeField]
-    private Item _item;
-    public Item item {
-        get => _item;
-        set => _item = value;
-    }
-
+[CreateAssetMenu(fileName = "WaterBottle", menuName="Item/Bottle of water")]
+public class WaterBottle : Item {
+    [Space(10)]
+    [Header("Water bottle properties")]
+    public int pointsOnDrink;
+    
     private PlayerController playerController;
-
-    private void Start() {
-        _item.onUse.AddListener(Drink);
-        playerController = PlayerController.instance;
-    }
 
     private void Drink() {
         Debug.Log("You drunk a bottle of water!");
-        playerController.GetComponent<ThirstController>().thirst += 20;
+        playerController = PlayerController.instance;
+        playerController.GetComponent<ThirstController>().thirst += pointsOnDrink;
+    }
+
+    public override void Use() {
+        Drink();
     }
 }
