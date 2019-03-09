8c5df87cbb9863266bb1722317f94fadbe6a68b6
Oskar <pawicao@gmail.com>
2019-03-09 18:58:13 +0100

    Health
    Basic death implemented

---
+++ Mars/Assets/Scripts/HealthController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+    public int maxHealth;
+    public int initHealth;
+    
+    private float _health;
+    public float health {
+        get => _health;
+        set => _health = value;
+    }
+
+    public static HealthController instance;
+    private HealthBar healthBar;
+
+    private void Awake() {
+        if (!instance)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Start() {
+        _health = initHealth;
+        healthBar = HealthBar.instance;
+    }
+
+    public void ChangeHealth(int diff)
+    {
+        if (_health + diff < 0)
+            _health = 0;
+        else
+            health += diff;
+        healthBar.ChangeHealthBar();
+        if (_health <= 0.0f) {
+            Debug.Log("You died");
+        }
+    }
+}
--- Mars/Assets/Scripts/HungerController.cs
+++ Mars/Assets/Scripts/HungerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HungerController : MonoBehaviour {
     public int maxHunger;
     public int initHunger;
+    public int starvationDamage;
+    public int recoverySpeed;
 
     private float _hunger;
     public float hunger {
@@ -17,6 +19,8 @@ public class HungerController : MonoBehaviour {
 
     public static HungerController instance;
 
+    private HealthController healthController;
+
     private void Awake() {
         if (!instance)
             instance = this;
@@ -26,13 +30,20 @@ public class HungerController : MonoBehaviour {
 
     private void Start() {
         _hunger = initHunger;
+        healthController = HealthController.instance;
     }
 
     private void Update() {
         _hunger -= dropSpeed * Time.deltaTime;
 
         if (_hunger <= 0.0f) {
-            Debug.Log("U ded");
+            Debug.Log("You are dying out of hunger!");
+            healthController.ChangeHealth(-starvationDamage);
+        }
+        else if (_hunger >= 0.9 * maxHunger)
+        {
+            Debug.Log("You are well fed, so you are slowly regaining your health.");
+            healthController.ChangeHealth(recoverySpeed);
         }
     }
 }
