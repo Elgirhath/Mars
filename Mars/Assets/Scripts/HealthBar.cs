7b589b4a2e3662adc5b28b18a64d0d1f938b6899
Oskar <pawicao@gmail.com>
2019-03-09 18:58:13 +0100

    Health
    Prosty pomysł na umieranie z głodu/pragnienia zaimplementowałem. Oczywiście trzeba będzie go odrobinę zmienić uwzględniając tempo tego umierania, ale mniej więcej zamysł ująłem w kodzie.

---
+++ Mars/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+    public int maxValue;
+    public int value;
+
+    private Text conditionText;
+    private Slider slider;
+
+    private HealthController healthController;
+    
+    public static HealthBar instance;
+
+    private void Awake() {
+        if (!instance)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        conditionText = GetComponentInChildren<Text>();
+        slider = GetComponentInChildren<Slider>();
+    }
+
+    private void Start() {
+        healthController = HealthController.instance;
+
+        maxValue = healthController.maxHealth;
+        value = (int) healthController.health;
+    }
+
+    public void ChangeHealthBar()
+    {
+        value = (int) healthController.health;
+        conditionText.text = value + "/" + maxValue;
+        slider.value = (float)value / maxValue;
+    }
+}
