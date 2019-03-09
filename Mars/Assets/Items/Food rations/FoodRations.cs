b13208a572952bb2d27b1e1f2036dd0985cffb97
Oskar <pawicao@gmail.com>
2019-03-09 13:23:58 +0100

    Food rations asset

---
+++ Mars/Assets/Items/Food rations/FoodRations.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "FoodRations", menuName="Item/Food rations")]
+public class FoodRations : Item {
+    [Space(10)]
+    [Header("Food rations properties")]
+    public int pointsOnEat;
+    
+    private PlayerController playerController;
+    private HungerController hungerController;
+
+    private void Eat() {
+        Debug.Log("You ate some food rations!");
+        hungerController = HungerController.instance;
+        hungerController.hunger += pointsOnEat;
+    }
+
+    public override void Use() {
+        Eat();
+    }
+}
