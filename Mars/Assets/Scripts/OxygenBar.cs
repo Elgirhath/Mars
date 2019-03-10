c8cf10ff70d9a1b7c4df36a512848a3ae2456283
Oskar <pawicao@gmail.com>
2019-03-10 18:13:05 +0100

    OxygenBar dodany
    W następnej kolejności ogarniam pasek zdrowia i wpływ pozostałych pasków na niego (żeby nie zapierdalał do nieskonczonosci chociazby xD)

---
+++ Mars/Assets/Scripts/OxygenBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenBar : MonoBehaviour {
+    public int maxValue;
+    public int value;
+
+    private Text conditionText;
+    private Slider slider;
+
+    public static OxygenBar instance;
+
+    private void Awake() {
+	    if (!instance)
+		    instance = this;
+	    else if (instance != this)
+		    Destroy(gameObject);
+	    
+        conditionText = GetComponentInChildren<Text>();
+        slider = GetComponentInChildren<Slider>();
+    }
+
+    private void Start() 
+    {
+	    
+    }
+
+    public void ChangeOxygenBar(int oxygen) {
+            value = oxygen;
+            conditionText.text = value + "/" + maxValue;
+            slider.value = (float)value / maxValue;
+    }
+}
---
+++ Mars/Assets/Scripts/OxygenController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenController : MonoBehaviour {
+    public int maxOxygen;
+    public int initOxygen;
+    public int chokingDamage;
+    [Tooltip("Points per second")]
+    public float dropSpeed;
+    
+    private float _oxygen;
+    public float oxygen {
+        get => _oxygen;
+        set => _oxygen = value;
+    }
+
+    private bool _insideCapsule;
+
+    public bool insideCapsule
+    {
+        get => _insideCapsule;
+        set => _insideCapsule = value;
+    }
+
+    public static OxygenController instance;
+    
+    private HealthController healthController;
+    private OxygenBar oxygenBar;
+
+    private void Awake() {
+        if (!instance)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    private void Start() {
+        _oxygen = initOxygen;
+        healthController = HealthController.instance;
+        oxygenBar = OxygenBar.instance;
+        insideCapsule = false;
+
+        oxygenBar.maxValue = maxOxygen;
+        oxygenBar.value = (int)oxygen;
+    }
+
+    private void Update()
+    {
+        if (!insideCapsule)
+        {
+            _oxygen -= dropSpeed * Time.deltaTime;
+            if (_oxygen <= 0.0f) {
+                Debug.Log("You are choking to death!");
+                healthController.ChangeHealth(-chokingDamage);
+            }
+            oxygenBar.ChangeOxygenBar((int) oxygen);
+        }
+    }
+}
