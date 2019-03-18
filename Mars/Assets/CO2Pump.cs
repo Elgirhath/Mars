72d8f9e7ee1e8f5d9a78e3f850cd8b9cb96b971e
konrad <konrad@polarczyk.com>
2019-03-19 00:23:32 +0100

    AirController
    Works, as well as O2 pump

---
+++ Mars/Assets/CO2Pump.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CO2Pump : MonoBehaviour {
+    public float kgPerSec;
+    public float activateThreshold;
+
+    private CapsuleAirController airController;
+    // Start is called before the first frame update
+    void Start() {
+        airController = GetComponentInParent<CapsuleAirController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Mathf.Abs(airController.co2Mass - airController.co2TargetMass) > activateThreshold) {
+            float maxAmount = airController.co2TargetMass - airController.co2Mass;
+            float amount = Mathf.Sign(maxAmount) * kgPerSec;
+            airController.co2Mass += Mathf.Clamp(amount * Time.deltaTime, -Mathf.Abs(maxAmount), Mathf.Abs(maxAmount));
+        }
+    }
+}
