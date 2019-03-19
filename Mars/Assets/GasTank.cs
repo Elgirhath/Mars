1bc0ee79b288eb3129c1e6e5b02910e5cbe15c59
Oskar <pawicao@gmail.com>
2019-03-19 10:41:01 +0100

    Revert "Revert "Merge chuj wie czy dobry""
    This reverts commit 63495f7feb90dd0ac5d6bd5fa859187ed86b51f9.

---
+++ Mars/Assets/GasTank.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasTank : MonoBehaviour {
+    public float maxGasWeight;
+    
+    [SerializeField]
+    private float _gasWeight;
+    public float gasWeight {
+        get {
+            return _gasWeight; 
+        }
+        set { _gasWeight = Mathf.Clamp(value, 0f, maxGasWeight); }
+    }
+}
