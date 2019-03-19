ed6eba0e49003355ebe50a4f5bbc40dd0e3ed9be
KonradPolarczyk <konradpolarczyk98@gmail.com>
2019-03-20 00:45:43 +0100

    Przepudowany system powietrza

---
+++ Mars/Assets/Air/Gases/Gas.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Gas", menuName="Gas")]
+public class Gas : ScriptableObject{
+	public float heatCapacity = 1012f; // J/kg*K
+	public float molarMass;
+	private float gasConstant = 8.3144598f;
+
+	public float GetMass(Air air) {
+		float totalAirMass = air.GetMassFromPressure();
+		float pseudoMass = air.gases[this] * molarMass;
+		float airMolarMass = air.GetMolarMass();
+		return pseudoMass / airMolarMass * totalAirMass;
+	}
+	
+	public void SetMass(Air air, float mass) {
+		float totalMass = air.GetMass() - GetMass(air) + mass;
+		float totalMoles = GetMoles(mass);
+		foreach (var gas in air.gases.Keys) {
+			if (gas == this) 
+				continue;
+			
+			totalMoles += gas.GetMoles(air);
+		}
+		
+		float volumeRatio = GetMoles(mass) / totalMoles;
+		Dictionary<Gas, float> newGases = new Dictionary<Gas, float>(air.gases);
+		newGases[this] = volumeRatio;
+		foreach (var pair in air.gases) {
+			if (pair.Key == this) 
+				continue;
+			volumeRatio = pair.Key.GetMoles(air) / totalMoles;
+			newGases[pair.Key] = volumeRatio;
+		}
+
+		air.gases = newGases;
+
+		float molarMass = air.GetMolarMass();
+		float density = totalMass / air.volume;
+		float tempAbsolute = air.GetTemperatureAbsolute();
+		float pressure = density * gasConstant * tempAbsolute / molarMass;
+		air.pressure = pressure;
+	}
+
+	public float GetMoles(Air air) {
+		return GetMass(air) / molarMass;
+	}
+	
+	public float GetMoles(float mass) {
+		return mass / molarMass;
+	}
+
+	public float GetVolume(Air air) {
+		float moles = GetMass(air) / molarMass;
+		float volumePct = moles / air.GetTotalMoles();
+		return volumePct * air.volume;
+	}
+	
+	public float GetVolume(Air air, float mass) {
+		float moles = mass / molarMass;
+		float totalMoles = air.GetTotalMoles() - GetMoles(air) + GetMoles(mass);
+		float volumePct = moles / totalMoles;
+		return volumePct * air.volume;
+	}
+}
--- Mars/Assets/Air/Heater.cs
+++ Mars/Assets/Air/Heater.cs
@@ -18,13 +18,14 @@ public class Heater : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (airController.temperature < airController.temperatureTarget) {
-            float diff = Mathf.Pow(temperature, 4) - Mathf.Pow(airController.temperature, 4);
+        if (airController.air.temperature < airController.targetAir.temperature) {
+            float airTemp = airController.air.temperature;
+            float diff = Mathf.Pow(temperature, 4) - Mathf.Pow(airTemp, 4);
             float radiativeHeat = area * boltzmanConstant * emmisivity * diff;
-            float convectiveHeat = convectiveCoefficient * area * (temperature - airController.temperature);
+            float convectiveHeat = convectiveCoefficient * area * (temperature - airTemp);
 
-            float dTdt = (radiativeHeat + convectiveHeat) / (airController.GetMass() * airController.heatCapacity);
-            airController.temperature = airController.temperature + dTdt * Time.deltaTime;
+            float dTdt = (radiativeHeat + convectiveHeat) / (airController.air.GetMass() * airController.air.heatCapacity);
+            airController.air.temperature += dTdt * Time.deltaTime;
         }
     }
 }
