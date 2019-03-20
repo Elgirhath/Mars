fef1b55efefa505a9ca4760976d66093abdb3ade
konrad <konrad@polarczyk.com>
2019-03-20 14:22:14 +0100

    Full air + control system
    Seems like capsule air is well implemented, control system included. Pumps and tanks for each gas work, O2 is adjusted based on partial pressure.
    CO2 based on gas volume ratio. N2 based on pressure. Also a heater with a constant temperature.

---
+++ Mars/Assets/Air/O2Pump.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class O2Pump : MonoBehaviour
+{
+    public float kgPerSec;
+    public float activateThreshold;
+
+    public Gas o2;
+    private GasTank tank;
+
+    private CapsuleAirController airController;
+    // Start is called before the first frame update
+    void Start() {
+        airController = GetComponentInParent<CapsuleAirController>();
+        tank = GetComponentInChildren<GasTank>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float currentPartialPressure = o2.GetPartialPressure(airController.air);
+        float targetPartialPressure = o2.GetPartialPressure(airController.targetAir);
+
+        if (Mathf.Abs(targetPartialPressure - currentPartialPressure) > activateThreshold) {
+            float amount = Mathf.Sign(targetPartialPressure - currentPartialPressure) * kgPerSec * Time.deltaTime;
+//            float amountClamped = Mathf.Clamp(amount, -Mathf.Abs(maxAmount), Mathf.Abs(maxAmount));
+            amount = amount > tank.gasWeight ? tank.gasWeight : amount;
+            float currentMass = o2.GetMass(airController.air);
+            float newMass = currentMass + amount;
+            o2.SetMass(airController.air, newMass);
+            tank.gasWeight -= amount;
+        }
+    }
+}
