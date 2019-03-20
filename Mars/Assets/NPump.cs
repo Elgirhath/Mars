d1aba31c0daf7827476b0acc2bdbf138672f7a89
konrad <konrad@polarczyk.com>
2019-03-20 14:22:14 +0100

    Air
    Wydaje mi się, że w miarę poprawnie zaimplementowane powietrze w kapsule, wraz z systemami jego kontroli.
    
    Działają pompy każdego gazu i zbiorniki gazów, tlen dopasowywany wg. ciśnienia cząstkowego, co2 wg. stosunku objętościowego gazów w kapsule, a n2 odpowiada głównie za ciśnienie w kabinie. Działa też grzejnik, który póki co ma stałą temperaturę (nie rozgrzewa się)

---
+++ Mars/Assets/NPump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPump : MonoBehaviour
+{
+    public float kgPerSec;
+    public float activateThreshold;
+
+    public Gas n2;
+    private GasTank tank;
+
+    private CapsuleAirController airController;
+    // Start is called before the first frame update
+    void Start() {
+        airController = GetComponentInParent<CapsuleAirController>();
+        tank = GetComponentInChildren<GasTank>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Air currentAir = airController.air;
+        Air targetAir = airController.targetAir;
+        if (Mathf.Abs(targetAir.pressure - currentAir.pressure) > activateThreshold) {
+            float amount = Mathf.Sign(targetAir.pressure - currentAir.pressure) * kgPerSec * Time.deltaTime;
+            amount = amount > tank.gasWeight ? tank.gasWeight : amount;
+            float currentMass = n2.GetMass(airController.air);
+            float newMass = currentMass + amount;
+            n2.SetMass(airController.air, newMass);
+            tank.gasWeight -= amount;
+        }
+    }
+}
