e234c54e02f264e3d1ef6f8555b3493ad0afbae1
konrad <konrad@polarczyk.com>
2019-03-21 01:36:29 +0100

    Power
    W miarę działający prąd i dystrybucja, do zaimplementowania działanie wszystkich sprzętów i może bezpieczników.

---
+++ Mars/Assets/PowerGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerGenerator : MonoBehaviour {
+    public float maxPower;
+
+    public float GetPower() {
+        return maxPower;
+    }
+}
---
+++ Mars/Assets/PowerSocket.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerSocket : MonoBehaviour {
+    private PowerSupply powerSupply;
+    public float powerConsumption;
+
+    public float powerReceived {
+        get => GetPower();
+    }
+    
+    public float GetPower() {
+        return powerSupply.GetPower(this);
+    }
+
+    private void Start() {
+        powerSupply = transform.parent.GetComponentInChildren<PowerSupply>();
+        powerSupply.receivers.Add(this);
+    }
+}
