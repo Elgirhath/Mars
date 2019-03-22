d52ce7b766b275c4615a265121672c0fa375a021
KonradPolarczyk <konradpolarczyk98@gmail.com>
2019-03-22 22:24:56 +0100

    Capsule air window

--- Mars/Assets/Air/O2Pump.cs
+++ Mars/Assets/Air/O2Pump.cs
@@ -12,25 +12,30 @@ public class O2Pump : MonoBehaviour
     private GasTank tank;
 
     private CapsuleAirController airController;
-    // Start is called before the first frame update
+    private PowerSocket powerSocket;
+    
     void Start() {
         airController = GetComponentInParent<CapsuleAirController>();
         tank = GetComponentInChildren<GasTank>();
+        powerSocket = GetComponent<PowerSocket>();
     }
 
     // Update is called once per frame
     void Update() {
+        if (!powerSocket.IsPowered())
+            return;
+        
         float currentPartialPressure = o2.GetPartialPressure(airController.air);
         float targetPartialPressure = o2.GetPartialPressure(airController.targetAir);
 
-        if (Mathf.Abs(targetPartialPressure - currentPartialPressure) > activateThreshold) {
-            float amount = Mathf.Sign(targetPartialPressure - currentPartialPressure) * kgPerSec * Time.deltaTime;
-//            float amountClamped = Mathf.Clamp(amount, -Mathf.Abs(maxAmount), Mathf.Abs(maxAmount));
-            amount = amount > tank.gasWeight ? tank.gasWeight : amount;
-            float currentMass = o2.GetMass(airController.air);
-            float newMass = currentMass + amount;
-            o2.SetMass(airController.air, newMass);
-            tank.gasWeight -= amount;
-        }
+        if (!(Mathf.Abs(targetPartialPressure - currentPartialPressure) > activateThreshold))
+            return;
+        
+        float amount = Mathf.Sign(targetPartialPressure - currentPartialPressure) * kgPerSec * Time.deltaTime;
+        amount = amount > tank.gasWeight ? tank.gasWeight : amount;
+        float currentMass = o2.GetMass(airController.air);
+        float newMass = currentMass + amount;
+        o2.SetMass(airController.air, newMass);
+        tank.gasWeight -= amount;
     }
 }
