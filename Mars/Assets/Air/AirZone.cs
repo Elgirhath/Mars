effe18c8813bd314769dbe27fa097a7aa14b6af9
KonradPolarczyk <konradpolarczyk98@gmail.com>
2019-08-27 21:00:06 +0200

    AirZone and refactors

---
+++ Mars/Assets/Air/AirZone.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirZone : MonoBehaviour
+{
+    [SerializeField]
+    public Air air;
+}
---
+++ Mars/Assets/Zone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Zone : MonoBehaviour
+{
+    public static T[] GetZonesInPoint<T>()
+    {
+        var allZones = GameObject.FindGameObjectsWithTag("Zone");
+        return allZones.Select(x => x.GetComponent<T>()).Where(x => x != null).ToArray();
+    }
+}
