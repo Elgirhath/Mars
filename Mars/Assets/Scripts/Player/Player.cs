d840c45181c0690b43b2768d4cdc4eec5d3ab4ac
KonradPolarczyk <konradpolarczyk98@gmail.com>
2020-05-08 23:03:24 +0200

    Energy refactors and fixes

--- Mars/Assets/Scripts/Player/Player.cs
+++ Mars/Assets/Scripts/Player/Player.cs
@@ -12,14 +12,11 @@ namespace Assets.Scripts.Player
         private Rigidbody rb;
         public float runSpeed;
         public float sensitivity;
-        private Transform tr;
         public float walkSpeed;
-        private bool locked;
-        public bool jumped;
+        public bool camLockState { get; set; }
 
         public static Player instance;
         private float moveSpeed;
-        private bool jumpedWhileSprinting;
         private EnergyController energyController;
 	
 
@@ -27,14 +24,11 @@ namespace Assets.Scripts.Player
         private void Start() {
             cam = GetComponentInChildren<Camera>();
             rb = GetComponent<Rigidbody>();
-            tr = transform;
             col = GetComponent<CapsuleCollider>();
+            energyController = GetComponent<EnergyController>();
 
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
-            jumped = false;
-            jumpedWhileSprinting = false;
-            energyController = EnergyController.instance;
         }
 
         private void Awake() {
@@ -46,79 +40,76 @@ namespace Assets.Scripts.Player
 
         private void Update()
         {
-            jumped = false;
             CamLook();
-            Jump();
+
+            if (Input.GetButtonDown("Jump") && IsGrounded())
+            {
+                Jump();
+            }
         }
 
         private void FixedUpdate() {
             Move();
         }
 
+
         private void CamLook() {
             //TODO: interpolate cam movement: http://www.kinematicsoup.com/news/2016/8/9/rrypp5tkubynjwxhxjzd42s3o034o8?fbclid=IwAR09F2udjxU9i_D7VuLsjHUXPco0tGQ4Gnf96emKsRcBNCt6ubW43xnlO-c
 
-            if (locked)
-                return;
+            if (camLockState) return;
 		
-            float mouseX = Input.GetAxis("Mouse X");
-            float mouseY = Input.GetAxis("Mouse Y");
+            var mouseX = Input.GetAxis("Mouse X");
+            var mouseY = Input.GetAxis("Mouse Y");
 
-            tr.RotateAround(tr.position, tr.up, mouseX * sensitivity);
+            transform.RotateAround(transform.position, transform.up, mouseX * sensitivity);
 
-            float currentAngle = Vector3.Angle(-tr.up, cam.transform.forward) - 90; //makes currentAngle [0,90] when looking up and [0,-90] when looking down
-            float angle = mouseY * sensitivity;
+            var currentAngle = Vector3.Angle(-transform.up, cam.transform.forward) - 90; //makes currentAngle [0,90] when looking up and [0,-90] when looking down
+            var angle = mouseY * sensitivity;
 
-            if (Mathf.Abs(currentAngle + angle) < angleLimit) {
-                Transform camTransform = cam.transform;
-                cam.transform.RotateAround(camTransform.position, -camTransform.right, angle);
-            }
+            if (!(Mathf.Abs(currentAngle + angle) < angleLimit)) return;
+
+            var camTransform = cam.transform;
+            cam.transform.RotateAround(camTransform.position, -camTransform.right, angle);
         }
 
         private void Move() {
-            float moveX = Input.GetAxisRaw("Horizontal");
-            float moveY = Input.GetAxisRaw("Vertical");
+            var moveX = Input.GetAxisRaw("Horizontal");
+            var moveY = Input.GetAxisRaw("Vertical");
 
-            Vector3 moveDirection = Vector3.Normalize(moveX * tr.right + moveY * tr.forward);
+            var moveDirection = Vector3.Normalize(moveX * transform.right + moveY * transform.forward);
 
             //check whether player is running and set his speed
-            if (!IsGrounded())
-            {
-                moveSpeed = jumpedWhileSprinting ? runSpeed : walkSpeed;
-            }
-            else
+            //during a jump moveSpeed remains unchanged
+            if (IsGrounded())
             {
                 moveSpeed = IsRunning() ? runSpeed : walkSpeed;
             }
-		
 
-            Vector3 moveVector = moveDirection * moveSpeed;
+            var moveVector = moveDirection * moveSpeed;
 
-            rb.velocity = moveVector + rb.velocity.y * tr.up; //moveVector is set in 2d, missing the Y axis, which should remain unchanged
+            rb.velocity = moveVector + rb.velocity.y * transform.up; //moveVector is set in 2d, missing the Y axis, which should remain unchanged
         }
 
-        private void Jump() {
-            if (Input.GetButtonDown("Jump") && IsGrounded())
-            {
-                jumped = true;
-                rb.velocity += Vector3.up * jumpHeight;
-                jumpedWhileSprinting = IsRunning();
-            }
+        private void Jump()
+        {
+            if (!energyController.canJump) return;
+            rb.velocity += Vector3.up * jumpHeight;
+            energyController.ExecuteJumpLoss();
         }
 
         public bool IsRunning()
         {
-            if (energyController.LockState) return false;
-            return Input.GetButton("Run");
+            return energyController.canRun && IsGrounded() && Input.GetButton("Run");
         }
 
-        public bool IsGrounded() {
-            return Physics.Raycast(tr.TransformPoint(col.center), -Vector3.up, col.height / 2 + 0.1f);
+        public bool IsGrounded()
+        {
+            return Physics.Raycast(transform.TransformPoint(col.center), -Vector3.up, col.height / 2 + 0.1f);
         }
 
-        public bool camLockState {
-            get { return locked; }
-            set { locked = value; }
+        public bool IsResting()
+        {
+            return IsGrounded() && !Input.GetButton("Run");
         }
     }
 }
